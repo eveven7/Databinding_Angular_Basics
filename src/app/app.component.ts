4a3a36b983e8de6c906945bd4153965657026386
Evelina <evelina.venckute@swedbank.lt>
2023-05-31 13:28:51 +0300

    components + output, input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [
+    { type: 'server', name: 'TestServer', content: 'just a test' },
+  ];
 
-  onAddServer() {
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent,
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(bluePrintData: {
+    newServerName: string;
+    newServerContent: string;
+  }) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: bluePrintData.newServerName,
+      content: bluePrintData.newServerContent,
     });
   }
 }
--- src/app/cockipit/cockipit.component.ts
+++ src/app/cockipit/cockipit.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockipit',
   templateUrl: './cockipit.component.html',
-  styleUrls: ['./cockipit.component.css']
+  styleUrls: ['./cockipit.component.css'],
 })
 export class CockipitComponent implements OnInit {
+  //output passing out of the component
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>(); //call a constructor of an emitter, created new emitter object
+  @Output() blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  newServerName = '';
+  newServerContent = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
+    });
   }
 
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent,
+    });
+  }
 }
